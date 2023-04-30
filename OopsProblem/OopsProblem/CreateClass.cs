97acaea45343d9e69897c32addb9edce8e1a7068
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-04-30 15:04:33 +0530

    [Ankita] Add : UC1-Example Of Class n Object

---
+++ OopsProblem/OopsProblem/CreateClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsProblem
+{
+    public class CreateClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Example Of Object class");
+        }
+
+    }
+}
---
+++ OopsProblem/OopsProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OopsProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateClass obj = new CreateClass();
+            obj.Method();
+        }
+    }
+}
